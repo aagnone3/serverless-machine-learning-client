b4ff9766244dfc06a360aafb7ccbf1e32f560fd1
Anthony Agnone <anthonyagnone@gmai.com>
2020-09-26 11:01:20 -0400

    [feat] set up initial structure for fake notes app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-api-dev-attachmentsbucket-zmimj85poxfm"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://i2x6me4gr7.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_9cdaNTnKJ",
+    APP_CLIENT_ID: "1omnod1vqiet9br6jh5iq62508",
+    IDENTITY_POOL_ID: "us-east-1:c18b1ceb-8149-48e7-b487-df5fc84f2cbd"
+  }
+};
